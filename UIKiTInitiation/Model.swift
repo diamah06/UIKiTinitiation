f609fb8e69aea9313f76a767aa230571ee6f2469
diamah06 <tabalmahdia@yahoo.fr>
2023-11-21 15:15:18 +0100

    first commit

---
+++ UIKiTInitiation/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  UIKiTInitiation
+//
+//  Created by Mahdia Amriou on 21/11/2023.
+//
+
+import Foundation
---
+++ UIKiTInitiation/Presenter.swift
@@ -0,0 +1,8 @@
+//
+//  Presenter.swift
+//  UIKiTInitiation
+//
+//  Created by Mahdia Amriou on 21/11/2023.
+//
+
+import Foundation
